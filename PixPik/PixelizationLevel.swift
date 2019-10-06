26b33d8d7e0d9a0bde62cfd9be9765c9b518226b
Darius Vilčinskis <darius.vilcinskis@gmail.com>
2019-10-06 15:02:58 +0300

    Change possible granularity

--- PixPik/PixelizationLevel.swift
+++ PixPik/PixelizationLevel.swift
@@ -3,12 +3,14 @@ enum PixelizationLevel: Int {
     case small
     case medium
     case large
+    case extraLarge
     
     var intensity: Int {
         switch self {
         case .small: return 10
-        case .medium: return 40
-        case .large: return 80
+        case .medium: return 25
+        case .large: return 50
+        case .extraLarge: return 70
         }
     }
     
