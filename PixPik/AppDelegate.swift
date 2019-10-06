3ba6be258859517165253cc65a79270a484f620a
Darius Vilčinskis <darius.vilcinskis@gmail.com>
2019-10-06 21:19:01 +0300

    Remove scene delegate

--- PixPik/AppDelegate.swift
+++ PixPik/AppDelegate.swift
@@ -2,9 +2,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: CameraViewController())
+        navigationController.setNavigationBarHidden(true, animated: false)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
